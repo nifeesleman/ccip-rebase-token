fe00786a0b90a64bc6aace2c7cbc0369967d87b8
nifeesleman <nsr-1868-14@amu.edu.et>
2025-07-14 23:36:54 +0300

    refactor: enhance code structure and add comments for clarity in RebaseToken contract

--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -1,7 +1,29 @@
 // SPDX-License-Identifier: MIT
 
+// Layout of Contract:
+// version
+// imports
+// interfaces, libraries, contracts
+// errors
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
 pragma solidity ^0.8.24;
 
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 /**
  * @title RebaseToken
  * @author Nife Esleman
@@ -10,7 +32,20 @@ pragma solidity ^0.8.24;
  * @notice Each user will have their own interest rate that is the global interest rate at the time of deposit.
  */
 
+contract RebaseToken is ERC20 {
+    error RebaseToken__InterestRateCanOnlyDecrease(uint256 oldInterestRate, uint256 newInterestRate);
 
-contract RebaseToken {
+    uint256 private s_interestRate= 5e10;
+    
+    event setInterestRate(uint256 newInterestRate);
+    
+    constructor() ERC20("Rebase Token", "RBT") {}
 
+    function setInterestRate(uint256 _newInterestRate) external {
+        // Set the interest rate
+        if(_newInterestRate > s_interestRate) {
+            revert RebaseToken__InterestRateCanOnlyDecrease(s_interestRate, _newInterestRate);
+        }
+        s_interestRate = _newInterestRate;
+        emit interestRateSet(_newInterestRate);
 }
