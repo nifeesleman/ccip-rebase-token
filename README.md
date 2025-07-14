681f2c455b7021af91c6a249d78d629b309565ec
nifeesleman <nsr-1868-14@amu.edu.et>
2025-07-15 00:25:58 +0300

    feat: add user-specific interest rate tracking and minting functionality in RebaseToken contract

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Cross-chain Rebase Token
 
 1. A protocol that allow user to deposite into a vault and in return, recieve rebase tokens that represent their underlying balance.
+
 2. Rebase token -> balanceOf function is dynammic to show the changing balance with time.
      - Balance increases linearly with time 
-     - Mint tokens to our users every time they perform an action(minting, burning, transferring, or.....bridging)
+     - Mint tokens to our users every time they perform an action(minting, burning, transferring, or.....bridging).
+     
 3. Interest rate
     - Indivuelly set an interest rate or each user based on some global interest rate of the protocol at the same time the user deposits into vault.
     - This global interest rate can only decrease to incetivese/reward early adopters.
--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -35,17 +35,43 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract RebaseToken is ERC20 {
     error RebaseToken__InterestRateCanOnlyDecrease(uint256 oldInterestRate, uint256 newInterestRate);
 
-    uint256 private s_interestRate= 5e10;
-    
+    uint256 private s_interestRate = 5e10;
+    mapping(address => uint256) private s_userInterestRate;
+
     event setInterestRate(uint256 newInterestRate);
-    
+
     constructor() ERC20("Rebase Token", "RBT") {}
 
     function setInterestRate(uint256 _newInterestRate) external {
         // Set the interest rate
-        if(_newInterestRate > s_interestRate) {
+        if (_newInterestRate > s_interestRate) {
             revert RebaseToken__InterestRateCanOnlyDecrease(s_interestRate, _newInterestRate);
         }
         s_interestRate = _newInterestRate;
         emit interestRateSet(_newInterestRate);
+    }
+
+    function mint(address _to, uint256 _amount) external {
+        _mintAccuredInterest(_to);
+        s_userInterestRate[_to] = s_interestRate;
+        // Mint tokens to the specified address
+        _mint(_to, _amount);
+    }
+
+function balanceOf(address _user) public view override returns (uint256) {
+    // Get the user's balance
+    return super.balanceOf(_user)*_calculateAccruedInterest(_user);
+}
+        
+function _mintAccuredInterest(address _user) internal {
+        
+/**
+ * @notice Gets the locked-in interest rate for a specific user.
+ * @param _user The address of the user.
+ * @return The user's specific interest rate.
+ */
+    function getUserInterestRate(address _user) external view returns (uint256) {
+        // Return the user's interest rate
+        // In this case, it is the global interest rate
+        return s_userInterestRate[_user];
 }
