cd7aa6f878ed100ea1735ee12589d0dafd51afd4
nifeesleman <nsr-1868-14@amu.edu.et>
2025-07-14 22:30:25 +0300

    refactor: restructure README and remove Counter contract files; add RebaseToken contract

---
+++ src/RebaseToken.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+/**
+ * @title RebaseToken
+ * @author Nife Esleman
+ * @notice This is a cross-chain rebase token that incentivises users to deposit into a vault and gain interest in rewards.
+ * @notice The interest rate in the smart contract can only decrease.
+ * @notice Each user will have their own interest rate that is the global interest rate at the time of deposit.
+ */
+
+
+contract RebaseToken {
+
+}
