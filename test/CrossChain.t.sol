9c1f47d9b86fe83f3cf82e91cc9fff9bf806bbf5
nifeesleman <nsr-1868-14@amu.edu.et>
2025-07-24 18:08:19 +0300

    feat: add TokenAdminRegistry for setPool

--- test/CrossChain.t.sol
+++ test/CrossChain.t.sol
@@ -57,6 +57,9 @@ contract CrossChainTest is Test {
             address(sepoliaToken)
         );
         TokenAdminRegistry(sepoliaNetworkDetails.tokenAdminRegistryAddress).acceptAdminRole(address(sepoliaToken));
+        TokenAdminRegistry(sepoliaNetworkDetails.tokenAdminRegistryAddress).setPool(
+            address(sepoliaToken), address(sepoliaPool)
+        );
 
         vm.stopPrank();
 
@@ -74,6 +77,9 @@ contract CrossChainTest is Test {
             address(arbSepoliaToken)
         );
         TokenAdminRegistry(arbSepoliaNetworkDetails.tokenAdminRegistryAddress).acceptAdminRole(address(arbSepoliaToken));
+        TokenAdminRegistry(arbSepoliaNetworkDetails.tokenAdminRegistryAddress).setPool(
+            address(arbSepoliaToken), address(arbSepoliaPool)
+        );
         vm.startPrank(owner);
         vm.stopPrank();
     }
